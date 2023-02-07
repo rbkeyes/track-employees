5b2f8faa343f0fd14e7bf42089c293d96d8f935b
Reed Keyes <rbkeyes@gmail.com>
2023-02-07 11:30:55 -0800

    added code to select employee data from db

--- db/queryTemplates.sql
+++ db/queryTemplates.sql
@@ -6,3 +6,13 @@ SELECT r.id, r.title, d.name AS department, r.salary
 FROM role as r
 LEFT JOIN department as d
 ON r.department_id = d.id;
+
+-- get employees: id, first and last name, title, department name, salary, manager
+SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT_WS(' ', m.first_name, m.last_name) AS manager
+FROM Employee e 
+LEFT JOIN role as r
+ON e.role_id = r.id
+LEFT JOIN department AS d
+ON r.department_id = d.id
+LEFT JOIN Employee m
+ON e.manager_id = m.id;
