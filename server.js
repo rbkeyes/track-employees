dc793800b65e54b7928ac775a2c47cd25352f9f1
Reed Keyes <rbkeyes@gmail.com>
2023-02-21 12:53:42 -0800

    pushed to main

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
-// import dependencies
-// const express = require('express');
-// const mysql = require('mysql2');
-// const consoleTable = require('console.table');
-// const inquirer = require('inquirer');
 const runPrompts = require("./utils/prompts");
 const Department = require('./lib/Department');
 const Role = require('./lib/Role');
 const Employee = require('./lib/Employee')
 const { viewData, modifyDb } = require('./utils/queries');
 const statements = require('./utils/preppedStatements');
-
-// register port and initiate app
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-
-// Express middleware
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
 
 // initiate application (async)
 const init = async () => {
